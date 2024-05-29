528055cab7b91f1ef8bfd2c1fb397f8e8786604f
Emin YTM <eminyetim25@gmail.com>
2024-05-29 19:04:44 +0300

    search in contacts

--- TelefonRehberi/Directory.xaml.cs
+++ TelefonRehberi/Directory.xaml.cs
@@ -1,15 +1,17 @@
 
 namespace TelefonRehberi;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Microsoft.Maui.Controls;
 using TelefonRehberi.Data;
 using TelefonRehberi.Models;
 
-
 public partial class Directory : ContentPage
 {
     public ObservableCollection<Contact> Contacts { get; set; }
+    public ObservableCollection<Contact> FilteredContacts { get; set; }
     public ICommand DeleteCommand { get; }
     private ContactDatabase _contactDatabase;
 
@@ -19,7 +21,8 @@ public partial class Directory : ContentPage
         _contactDatabase = new ContactDatabase();
 
         Contacts = new ObservableCollection<Contact>();
-        contactsCollectionView.ItemsSource = Contacts;
+        FilteredContacts = new ObservableCollection<Contact>();
+        contactsCollectionView.ItemsSource = FilteredContacts;
 
         DeleteCommand = new Command<Contact>(async (contact) => await OnDelete(contact));
 
@@ -34,6 +37,7 @@ public partial class Directory : ContentPage
             if (Contacts.Contains(contact))
             {
                 Contacts.Remove(contact);
+                FilteredContacts.Remove(contact);
                 await _contactDatabase.DeleteContactAsync(contact);
             }
         }
@@ -44,19 +48,54 @@ public partial class Directory : ContentPage
         base.OnAppearing();
         var contactList = await _contactDatabase.GetContactsAsync();
         Contacts.Clear();
+        FilteredContacts.Clear();
         foreach (var contact in contactList)
         {
             Contacts.Add(contact);
+            FilteredContacts.Add(contact);
+        }
+    }
+
+    private void OnSearchBarTextChanged(object sender, TextChangedEventArgs e)
+    {
+        FilterContacts(e.NewTextValue);
+    }
+
+    private void FilterContacts(string searchText)
+    {
+        if (string.IsNullOrWhiteSpace(searchText))
+        {
+            foreach (var contact in Contacts)
+            {
+                if (!FilteredContacts.Contains(contact))
+                {
+                    FilteredContacts.Add(contact);
+                }
+            }
+        }
+        else
+        {
+            var lowerCaseSearchText = searchText.ToLower();
+            var filteredItems = Contacts.Where(c => c.Name.ToLower().Contains(lowerCaseSearchText) ||
+                                                    c.PhoneNumber.Contains(searchText)).ToList();
+
+            // Update the FilteredContacts collection
+            FilteredContacts.Clear();
+            foreach (var item in filteredItems)
+            {
+                FilteredContacts.Add(item);
+            }
         }
     }
 
     private async void OnUpdateButtonClicked(object sender, EventArgs e)
     {
-        var button = sender as Button;
+        var button = sender as ImageButton;
         var contact = button?.BindingContext as Contact;
         if (contact != null)
         {
             await Navigation.PushAsync(new UpdateContactPage(contact));
         }
     }
+
 }
